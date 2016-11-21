ff07c3ddc53e9b19c7f543260dc6e23314b36d5a
heatherherrington <redherring@gmail.com>
2016-11-21 15:32:37 -0800

    Commented code re spacebar and return key issues

--- noise.js
+++ noise.js
@@ -78,7 +78,22 @@ $(document).ready( function() {
     playB();
   });
 
+  // Once a button has focus, the default action is, on keyup for 'spacebar', to play that note.
+  // This also happens on keydown 'return'. I tried to overcome this by doing both the function
+  // below and doing ' && event.which != 13' in the keydown functions below - neither worked.
+  // So, it will play the sound both when the required letter key is pressed, and when spacebar
+  // and return are pressed, depending on which button has focus.
+  // This annoys me, but I couldn't figure out how to fix it.
+
+  // var spaceBar = function() {
+  //   $('body').keyup(function(event) {
+  //     if (event.which == 32)
+  //     preventDefault();
+  //   });
+  // };
+
   $('body').keydown(function(event) {
+    // spaceBar();
     if (event.key == 'c')
     playC();
   });
