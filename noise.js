054257ae4ce9d8fe3c73a56b47d53c1a440e449a
heatherherrington <redherring@gmail.com>
2016-11-21 15:07:27 -0800

    Keyboard functionality working, refactored, WIP for spacebar and return keys

--- noise.js
+++ noise.js
@@ -8,56 +8,108 @@ $(document).ready( function() {
   var audioA = $('#aAudio')[0];
   var audioB = $('#bAudio')[0];
 
-  var aC = function() {
+  var playC = function() {
     audioC.pause();
     audioC.currentTime = 0;
     audioC.play();
   };
 
-  $('.c').click(function() {
-    aC();
-  });
-
-  $('.d').click(function() {
+  var playD = function() {
     audioD.pause();
     audioD.currentTime = 0;
     audioD.play();
-  });
+  };
 
-  $('.e').click(function() {
+  var playE = function() {
     audioE.pause();
     audioE.currentTime = 0;
     audioE.play();
-  });
+  };
 
-  $('.f').click(function() {
+  var playF = function() {
     audioF.pause();
     audioF.currentTime = 0;
     audioF.play();
-  });
+  };
 
-  $('.g').click(function() {
+  var playG = function() {
     audioG.pause();
     audioG.currentTime = 0;
     audioG.play();
-  });
+  };
 
-  $('.a').click(function() {
+  var playA = function() {
     audioA.pause();
     audioA.currentTime = 0;
     audioA.play();
-  });
+  };
 
-  $('.b').click(function() {
+  var playB = function() {
     audioB.pause();
     audioB.currentTime = 0;
     audioB.play();
+  };
+
+  $('.c').click(function() {
+    playC();
+  });
+
+  $('.d').click(function() {
+    playD();
+  });
+
+  $('.e').click(function() {
+    playE();
+  });
+
+  $('.f').click(function() {
+    playF();
+  });
+
+  $('.g').click(function() {
+    playG();
+  });
+
+  $('.a').click(function() {
+    playA();
+  });
+
+  $('.b').click(function() {
+    playB();
   });
 
   $('body').keydown(function(event) {
-    if (event.which == 'a') //'which' is better than 'keyCode' because 'which' also works with mouse; 32 = space; 13 = enter
-    audioA.pause();
-    audioA.currentTime = 0;
-    audioA.play();
+    if (event.key == 'c' && event.which != 32)
+    playC();
+  });
+
+  $('body').keydown(function(event) {
+    if (event.key == 'd')
+    playD();
+  });
+
+  $('body').keydown(function(event) {
+    if (event.key == 'e')
+    playE();
+  });
+
+  $('body').keydown(function(event) {
+    if (event.key == 'f')
+    playF();
+  });
+
+  $('body').keydown(function(event) {
+    if (event.key == 'g')
+    playG();
+  });
+
+  $('body').keydown(function(event) {
+    if (event.key == 'a')
+    playA();
+  });
+
+  $('body').keydown(function(event) {
+    if (event.key == 'b')
+    playB();
   });
 });
