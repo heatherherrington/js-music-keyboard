be546da5abebe2322b475bd09cbdb91d05c20ef4
heatherherrington <redherring@gmail.com>
2016-11-21 16:39:40 -0800

    Refactored button functionality

--- noise.js
+++ noise.js
@@ -1,81 +1,12 @@
 $(document).ready( function() {
 
-  var audioC = $('#cAudio')[0];
-  var audioD = $('#dAudio')[0];
-  var audioE = $('#eAudio')[0];
-  var audioF = $('#fAudio')[0];
-  var audioG = $('#gAudio')[0];
-  var audioA = $('#aAudio')[0];
-  var audioB = $('#bAudio')[0];
-
-  var playC = function() {
-    audioC.pause();
-    audioC.currentTime = 0;
-    audioC.play();
-  };
-
-  var playD = function() {
-    audioD.pause();
-    audioD.currentTime = 0;
-    audioD.play();
-  };
-
-  var playE = function() {
-    audioE.pause();
-    audioE.currentTime = 0;
-    audioE.play();
-  };
-
-  var playF = function() {
-    audioF.pause();
-    audioF.currentTime = 0;
-    audioF.play();
-  };
-
-  var playG = function() {
-    audioG.pause();
-    audioG.currentTime = 0;
-    audioG.play();
-  };
-
-  var playA = function() {
-    audioA.pause();
-    audioA.currentTime = 0;
-    audioA.play();
-  };
-
-  var playB = function() {
-    audioB.pause();
-    audioB.currentTime = 0;
-    audioB.play();
-  };
-
-  $('.c').click(function() {
-    playC();
-  });
-
-  $('.d').click(function() {
-    playD();
-  });
-
-  $('.e').click(function() {
-    playE();
-  });
-
-  $('.f').click(function() {
-    playF();
-  });
-
-  $('.g').click(function() {
-    playG();
-  });
-
-  $('.a').click(function() {
-    playA();
-  });
-
-  $('.b').click(function() {
-    playB();
+  $('.instrument').on('click', 'button', function() {
+    var playClass = this.className.slice(-1);
+    var playId = '#' + playClass + 'Audio';
+    var note = $(playId)[0];
+    note.pause();
+    note.currentTime = 0;
+    note.play();
   });
 
   // Once a button has focus, the default action is, on keyup for 'spacebar', to play that note.
